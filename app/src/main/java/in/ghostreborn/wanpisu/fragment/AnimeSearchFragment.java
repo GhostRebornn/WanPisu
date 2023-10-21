6271daf57c08da7352b6c66840b6f7b38f6654a0
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-21 16:46:43 +0530

    Update UI

--- app/src/main/java/in/ghostreborn/wanpisu/fragment/AnimeSearchFragment.java
+++ app/src/main/java/in/ghostreborn/wanpisu/fragment/AnimeSearchFragment.java
@@ -5,22 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import in.ghostreborn.wanpisu.R;
 import in.ghostreborn.wanpisu.parser.AllAnimeParser;
 
 public class AnimeSearchFragment extends Fragment {
 
-    TextView testText;
+    RecyclerView allAnimeRecycler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_anime_search,container, false);
-        testText = view.findViewById(R.id.test_text);
+        allAnimeRecycler = view.findViewById(R.id.all_anime_recycler);
         new AnimeSearchAsync().execute();
         return view;
     }
@@ -35,7 +35,6 @@ public class AnimeSearchFragment extends Fragment {
         @Override
         protected void onPostExecute(String output) {
             super.onPostExecute(output);
-            testText.setText(output);
         }
     }
 }
