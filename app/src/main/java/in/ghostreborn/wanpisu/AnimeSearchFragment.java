2fd4cfa9251da4d2061e5e8b014ab1b885d8cba4
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-21 11:14:18 +0530

    Show search bar

--- app/src/main/java/in/ghostreborn/wanpisu/AnimeSearchFragment.java
+++ app/src/main/java/in/ghostreborn/wanpisu/AnimeSearchFragment.java
@@ -13,6 +13,7 @@ public class AnimeSearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_anime_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_anime_search,container, false);
+        return view;
     }
 }
