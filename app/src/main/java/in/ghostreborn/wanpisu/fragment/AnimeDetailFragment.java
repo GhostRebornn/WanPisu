7e96d5aac1ab836353fd25ce844d8b6596a48b42
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-22 01:12:00 +0530

    Watch button

--- app/src/main/java/in/ghostreborn/wanpisu/fragment/AnimeDetailFragment.java
+++ app/src/main/java/in/ghostreborn/wanpisu/fragment/AnimeDetailFragment.java
@@ -1,11 +1,13 @@
 package in.ghostreborn.wanpisu.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,21 +23,32 @@ import in.ghostreborn.wanpisu.R;
 import in.ghostreborn.wanpisu.constants.Constants;
 import in.ghostreborn.wanpisu.model.Details;
 import in.ghostreborn.wanpisu.parser.AllAnimeParser;
+import in.ghostreborn.wanpisu.ui.EpisodeActivity;
 
 public class AnimeDetailFragment extends Fragment {
 
     TextView detailNameText;
     TextView detailDescText;
     ImageView detailImageView;
+    Button watchButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_anime_detail, container, false);
+        findViews(view);
+        return view;
+    }
+
+    private void findViews(View view) {
         detailNameText = view.findViewById(R.id.detail_name_text);
         detailDescText = view.findViewById(R.id.detail_desc_text);
         detailImageView = view.findViewById(R.id.detail_image_view);
-        return view;
+        watchButton = view.findViewById(R.id.watch_buton);
+
+        watchButton.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), EpisodeActivity.class));
+        });
     }
 
     @Override
---
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/EpisodeActivity.java
@@ -0,0 +1,16 @@
+package in.ghostreborn.wanpisu.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import in.ghostreborn.wanpisu.R;
+
+public class EpisodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_episode);
+    }
+}
