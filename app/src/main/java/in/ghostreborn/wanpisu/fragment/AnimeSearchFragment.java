bf62adad6854e7871e5a16693ff3c8316c97e19c
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-21 11:17:16 +0530

    Refactor and Code cleanup

--- app/src/main/java/in/ghostreborn/wanpisu/fragment/AnimeSearchFragment.java
+++ app/src/main/java/in/ghostreborn/wanpisu/fragment/AnimeSearchFragment.java
@@ -1,4 +1,4 @@
-package in.ghostreborn.wanpisu;
+package in.ghostreborn.wanpisu.fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import in.ghostreborn.wanpisu.R;
+
 public class AnimeSearchFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_anime_search,container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_anime_search,container, false);
     }
 }
