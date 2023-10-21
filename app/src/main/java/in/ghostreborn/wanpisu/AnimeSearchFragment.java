875138677c8dd30c03f969a3938d55d7c2690875
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-10-21 11:07:48 +0530

    Show blank fragment

---
+++ app/src/main/java/in/ghostreborn/wanpisu/AnimeSearchFragment.java
@@ -0,0 +1,18 @@
+package in.ghostreborn.wanpisu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AnimeSearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_anime_search, container, false);
+    }
+}
