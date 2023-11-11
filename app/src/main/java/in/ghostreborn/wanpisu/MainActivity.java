fb531560763f68dced90033d64752a79c3988ae6
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-11-11 13:52:22 +0530

    Don't reload fragment

--- app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
@@ -1,14 +1,52 @@
 package in.ghostreborn.wanpisu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import in.ghostreborn.wanpisu.fragment.AnimeSearchFragment;
+import in.ghostreborn.wanpisu.fragment.UserAnimeFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment;
+    int previouslySelected = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigationView = findViewById(R.id.main_bottom_navigation);
+        navigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == previouslySelected){
+                return true;
+            }
+
+            if (itemId == R.id.navigation_home){
+                fragment = new AnimeSearchFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_fragment_container, fragment)
+                        .commit();
+            }else if (itemId == R.id.navigation_user){
+                fragment = new UserAnimeFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_fragment_container, fragment)
+                        .commit();
+            }
+            previouslySelected = itemId;
+            return true;
+        });
+
     }
+
+
+
+
 }
