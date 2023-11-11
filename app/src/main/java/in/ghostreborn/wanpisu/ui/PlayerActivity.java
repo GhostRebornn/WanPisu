0fec7dbf42662ec7e7563d3f218c27370231ec69
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-11-11 09:03:00 +0530

    Play Anime * Move to Media3 ExoPlayer

--- app/src/main/java/in/ghostreborn/wanpisu/ui/PlayerActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/PlayerActivity.java
@@ -1,16 +1,33 @@
 package in.ghostreborn.wanpisu.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.media3.common.MediaItem;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.ui.PlayerView;
+
 import in.ghostreborn.wanpisu.R;
+import in.ghostreborn.wanpisu.constants.Constants;
 
 public class PlayerActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        PlayerView playerView = findViewById(R.id.player_view);
+
+        ExoPlayer player = new ExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+
+        MediaItem mediaItem = MediaItem.fromUri(Constants.ANIME_SERVER);
+        player.setMediaItem(mediaItem);
+        player.prepare();
+        player.play();
+
     }
+
 }
