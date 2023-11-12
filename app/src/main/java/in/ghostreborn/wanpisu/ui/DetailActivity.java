f7d0243a5f748c8f6bbd2f77d0ae9d06c9ab92f8
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-11-13 04:59:25 +0530

    JikanFragment

--- app/src/main/java/in/ghostreborn/wanpisu/ui/DetailActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/DetailActivity.java
@@ -1,8 +1,10 @@
 package in.ghostreborn.wanpisu.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,12 +18,14 @@ import java.util.concurrent.Executors;
 import in.ghostreborn.wanpisu.R;
 import in.ghostreborn.wanpisu.constants.Constants;
 import in.ghostreborn.wanpisu.model.Jikan;
+import in.ghostreborn.wanpisu.parser.AllAnimeParser;
 import in.ghostreborn.wanpisu.parser.JikanParser;
 
 public class DetailActivity extends AppCompatActivity {
 
     TextView detailNameText;
     ImageView detailImageView;
+    Button watchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +34,22 @@ public class DetailActivity extends AppCompatActivity {
 
         detailNameText = findViewById(R.id.detail_name_text);
         detailImageView = findViewById(R.id.detail_image_view);
+        watchButton = findViewById(R.id.watch_button);
+        getEpisodes();
         getDetails();
 
+    }
+
+    private void getEpisodes(){
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(() -> {
+            AllAnimeParser.getEpisodes(Constants.ANIME_ID);
+            handler.post(() -> watchButton.setOnClickListener(v -> startActivity(new Intent(
+                    DetailActivity.this,
+                    EpisodeActivity.class
+            ))));
+        });
     }
 
     private void getDetails() {
