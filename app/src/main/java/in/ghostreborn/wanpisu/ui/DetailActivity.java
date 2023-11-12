a3d93a2d18d5c71d73861bb7cf13b11781a7ed4f
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-11-13 05:20:02 +0530

    Show AnimeDetailFragment if malID is null

--- app/src/main/java/in/ghostreborn/wanpisu/ui/DetailActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/DetailActivity.java
@@ -1,68 +1,32 @@
 package in.ghostreborn.wanpisu.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.squareup.picasso.Picasso;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import in.ghostreborn.wanpisu.R;
 import in.ghostreborn.wanpisu.constants.Constants;
-import in.ghostreborn.wanpisu.model.Jikan;
-import in.ghostreborn.wanpisu.parser.AllAnimeParser;
-import in.ghostreborn.wanpisu.parser.JikanParser;
+import in.ghostreborn.wanpisu.fragment.AnimeDetailFragment;
+import in.ghostreborn.wanpisu.fragment.JikanFragment;
 
 public class DetailActivity extends AppCompatActivity {
-
-    TextView detailNameText;
-    ImageView detailImageView;
-    Button watchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        detailNameText = findViewById(R.id.detail_name_text);
-        detailImageView = findViewById(R.id.detail_image_view);
-        watchButton = findViewById(R.id.watch_button);
-        getEpisodes();
-        getDetails();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        if (Constants.ANIME_MAL_ID.equals("null")){
+            transaction.add(R.id.details_frame_layout, new AnimeDetailFragment());
+        }else {
+            transaction.add(R.id.details_frame_layout, new JikanFragment());
+        }
+        transaction.commit();
 
-    }
-
-    private void getEpisodes(){
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Handler handler = new Handler(Looper.getMainLooper());
-        executor.execute(() -> {
-            AllAnimeParser.getEpisodes(Constants.ANIME_ID);
-            handler.post(() -> watchButton.setOnClickListener(v -> startActivity(new Intent(
-                    DetailActivity.this,
-                    EpisodeActivity.class
-            ))));
-        });
-    }
-
-    private void getDetails() {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Handler handler = new Handler(Looper.getMainLooper());
-        executor.execute(() -> {
-            JikanParser.parseAnimeFull(Constants.ANIME_MAL_ID);
-            handler.post(() -> {
-                Jikan jikan = Constants.jikan;
-                detailNameText.setText(jikan.getTitle());
-                Picasso.get().load(jikan.getThumbnail()).into(detailImageView);
-            });
-        });
     }
 
 }
