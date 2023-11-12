528c091c07384695fb4d67443040793b5ce323e8
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-11-12 10:49:21 +0530

    Immersive Mode and Stay awake

--- app/src/main/java/in/ghostreborn/wanpisu/ui/PlayerActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/ui/PlayerActivity.java
@@ -1,6 +1,7 @@
 package in.ghostreborn.wanpisu.ui;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +20,7 @@ public class PlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setImmersiveMode();
 
         PlayerView playerView = findViewById(R.id.player_view);
 
@@ -31,6 +32,17 @@ public class PlayerActivity extends AppCompatActivity {
         player.prepare();
         player.play();
 
+    }
+
+    private void setImmersiveMode(){
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+        getWindow().setAttributes(params);
     }
 
 }
