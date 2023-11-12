a04029babe166b55e9d5379127de5a1774547c52
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-11-13 05:09:08 +0530

    AnimeDetailFragment

---
+++ app/src/main/java/in/ghostreborn/wanpisu/fragment/AnimeDetailFragment.java
@@ -0,0 +1,19 @@
+package in.ghostreborn.wanpisu.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import in.ghostreborn.wanpisu.R;
+
+public class AnimeDetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_anime_detail, container, false);
+    }
+}
