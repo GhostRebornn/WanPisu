d02f3a68450c1851a548ade40ddc1e840bc74c78
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-11-13 06:57:21 +0530

    Parse jikan details

--- app/src/main/java/in/ghostreborn/wanpisu/fragment/JikanFragment.java
+++ app/src/main/java/in/ghostreborn/wanpisu/fragment/JikanFragment.java
@@ -1,19 +1,96 @@
 package in.ghostreborn.wanpisu.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import in.ghostreborn.wanpisu.R;
+import in.ghostreborn.wanpisu.constants.Constants;
+import in.ghostreborn.wanpisu.model.Jikan;
+import in.ghostreborn.wanpisu.parser.AllAnimeParser;
 
 public class JikanFragment extends Fragment {
+
+    ImageView jikanAnimeImageView;
+    TextView jikanAnimeNameText;
+    TextView jikanAnimeTitlesText;
+    TextView jikanAnimeTypeText;
+    TextView jikanAnimeSourceText;
+    TextView jikanAnimeEpisodesText;
+    TextView jikanAnimeStatusText;
+    TextView jikanAnimeAiredText;
+    TextView jikanAnimeDurationText;
+    TextView jikanAnimeRatingText;
+    TextView jikanAnimeScoreText;
+    TextView jikanAnimeSeasonText;
+    TextView jikanAnimeBroadcastText;
+    TextView jikanAnimeSynopsisText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_jikan, container, false);
+        View view = inflater.inflate(R.layout.fragment_jikan, container, false);
+        findViews(view);
+        getJikans();
+        return view;
     }
+
+    private void findViews(View view){
+        jikanAnimeImageView = view.findViewById(R.id.jikan_anime_image_view);
+        jikanAnimeNameText = view.findViewById(R.id.jikan_anime_name_text);
+        jikanAnimeTitlesText = view.findViewById(R.id.jikan_anime_titles_text);
+        jikanAnimeTypeText = view.findViewById(R.id.jikan_anime_type_text);
+        jikanAnimeSourceText = view.findViewById(R.id.jikan_anime_source_text);
+        jikanAnimeEpisodesText = view.findViewById(R.id.jikan_anime_episodes_text);
+        jikanAnimeStatusText = view.findViewById(R.id.jikan_anime_status_text);
+        jikanAnimeAiredText = view.findViewById(R.id.jikan_anime_aired_text);
+        jikanAnimeDurationText = view.findViewById(R.id.jikan_anime_duration_text);
+        jikanAnimeRatingText = view.findViewById(R.id.jikan_anime_rating_text);
+        jikanAnimeScoreText = view.findViewById(R.id.jikan_anime_score_text);
+        jikanAnimeSeasonText = view.findViewById(R.id.jikan_anime_season_text);
+        jikanAnimeBroadcastText = view.findViewById(R.id.jikan_anime_broadcast_text);
+        jikanAnimeSynopsisText = view.findViewById(R.id.jikan_anime_synopsis_text);
+    }
+
+    private void getJikans() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(() -> {
+            AllAnimeParser.getEpisodes(Constants.ANIME_ID, false);
+            handler.post(() -> {
+                Jikan jikan = Constants.jikan;
+                Picasso.get().load(jikan.getThumbnail()).into(jikanAnimeImageView);
+                jikanAnimeNameText.setText(jikan.getTitle());
+                StringBuilder titles = new StringBuilder();
+                for (String title: jikan.getTitles()){
+                    titles.append(title).append(" ");
+                }
+                jikanAnimeTitlesText.setText(titles);
+                jikanAnimeTypeText.setText(jikan.getType());
+                jikanAnimeSourceText.setText(jikan.getSource());
+                jikanAnimeEpisodesText.setText(jikan.getEpisodes());
+                jikanAnimeStatusText.setText(jikan.getStatus());
+                jikanAnimeAiredText.setText(jikan.getAired());
+                jikanAnimeDurationText.setText(jikan.getDuration());
+                jikanAnimeRatingText.setText(jikan.getRating());
+                jikanAnimeScoreText.setText(jikan.getScore());
+                jikanAnimeSeasonText.setText(jikan.getSeason());
+                jikanAnimeBroadcastText.setText(jikan.getBroadcast());
+                jikanAnimeSynopsisText.setText(jikan.getSynopsis());
+            });
+        });
+    }
+
 }
